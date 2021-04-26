e9f4ab3705f4b6568f860e26f9990ce6bda9a887
pedro sousa <pedro.p.sousa@sapo.pt>
2021-04-26 01:01:01 +0000

    Pedro adds a method to reverse string and if statement to compare the original string object with the reversed string object

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Enter username");
 
-        String userName = myObj.nextLine();  // Read user input
-        System.out.println("Username is: " + userName);  // Output user input
+        Scanner userInput = new Scanner(System.in); // Receive user input ere
+        System.out.println("Insert a word");
+
+
+        String stringHolder = userInput.nextLine(); // Read user input "RACECAR"
+        String reversedString = "";
+        System.out.println("You've inserted: " + stringHolder); // Output user input "RACECAR"
+
+        int stringSize = stringHolder.length(); // 3
+
+        for (int i = stringSize-1; i >= 0; i--) {
+            reversedString = reversedString + stringHolder.charAt(i);
+        }
+        System.out.println(stringHolder + " reversed is: " + reversedString);
+
+        if (stringHolder.equals(reversedString)) {
+            System.out.println("Is palindrome!");
+        }
+        else {
+        System.out.println("Is not palindrome.");
+        }
     }
 }
