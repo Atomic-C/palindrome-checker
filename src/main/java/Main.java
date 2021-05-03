f24a4a692d254458336b8a0ab7e8bcbd8bb80abf
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-05-03 23:14:19 +0000

    Pedro fully migrates Palindrome checker logic into PalindromeV1 class, program is working as before changes.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,35 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Main something = new Main();
-        something.isPalindrome();
+        PalindromeV1 palindromeChecker = new PalindromeV1(); // Creates new instance of PalindromeV1
+        palindromeChecker.isPalindrome(); // Calls method isPalindrome() of palindromeChecker of type PalindromeV1
+
     }
-
-
-
-        public boolean isPalindrome() {
-
-            Scanner userInput = new Scanner(System.in); // Receive user input ere
-            System.out.println("Insert a word");
-
-
-            String stringHolder = userInput.nextLine().toLowerCase(); // Read user input "RACECAR"
-            String reversedString = "";
-            System.out.println("You've inserted: " + stringHolder); // Output user input "RACECAR"
-
-            int stringSize = stringHolder.length(); // 2
-
-            for (int i = stringSize - 1; i >= 0; i--) {
-                reversedString = reversedString + stringHolder.charAt(i);
-            }
-            System.out.println(stringHolder + " reversed is: " + reversedString);
-
-            if (stringHolder.equals(reversedString)) {
-                System.out.println("Is palindrome!");
-                return true;
-            } else {
-                System.out.println("Is not palindrome.");
-                return false;
-            }
-        }
 }
--- src/main/java/PalindromeV1.java
+++ src/main/java/PalindromeV1.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class PalindromeV1 {
 
-    // Move code from main here, and start TDD tom.
+    public boolean isPalindrome() {
+
+        Scanner userInput = new Scanner(System.in); // Receive user input ere
+        System.out.println("Insert a word");
+
+
+        String stringHolder = userInput.nextLine().toLowerCase(); // Read user input "RACECAR"
+        String reversedString = "";
+        System.out.println("You've inserted: " + stringHolder); // Output user input "RACECAR"
+
+        int stringSize = stringHolder.length(); // 2
+
+        for (int i = stringSize - 1; i >= 0; i--) {
+            reversedString = reversedString + stringHolder.charAt(i);
+        }
+        System.out.println(stringHolder + " reversed is: " + reversedString);
+
+        if (stringHolder.equals(reversedString)) {
+            System.out.println("Is palindrome!");
+            return true;
+        } else {
+            System.out.println("Is not palindrome.");
+            return false;
+        }
+    }
 
 }
