29b949c0eb36dc9bfd94fffee29c8f750114f1ba
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-05-03 23:35:51 +0000

    Pedro adds Test folder and a work in progress test, with maven dependency.

---
+++ src/test/java/PalindromeV1Test.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PalindromeV1Test {
+
+@Test
+public boolean isPalindrome() {
+    String inPut;
+    inPut = "madam";
+    assertEquals("madam",inPut);
+}
+
+}
