f5b35da726fb8467818791ee4d9614c968365aa4
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-05-03 23:07:07 +0000

    Pedro separates Palindrome checker logic from Main class to a separate class and instantiates it.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,26 +3,35 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner userInput = new Scanner(System.in); // Receive user input ere
-        System.out.println("Insert a word");
-
-
-        String stringHolder = userInput.nextLine().toLowerCase(); // Read user input "RACECAR"
-        String reversedString = "";
-        System.out.println("You've inserted: " + stringHolder); // Output user input "RACECAR"
-
-        int stringSize = stringHolder.length(); // 2
-
-        for (int i = stringSize-1; i >= 0; i--) {
-            reversedString = reversedString + stringHolder.charAt(i);
-        }
-        System.out.println(stringHolder + " reversed is: " + reversedString);
-
-        if (stringHolder.equals(reversedString)) {
-            System.out.println("Is palindrome!");
-        }
-        else {
-        System.out.println("Is not palindrome.");
-        }
+        Main something = new Main();
+        something.isPalindrome();
     }
+
+
+
+        public boolean isPalindrome() {
+
+            Scanner userInput = new Scanner(System.in); // Receive user input ere
+            System.out.println("Insert a word");
+
+
+            String stringHolder = userInput.nextLine().toLowerCase(); // Read user input "RACECAR"
+            String reversedString = "";
+            System.out.println("You've inserted: " + stringHolder); // Output user input "RACECAR"
+
+            int stringSize = stringHolder.length(); // 2
+
+            for (int i = stringSize - 1; i >= 0; i--) {
+                reversedString = reversedString + stringHolder.charAt(i);
+            }
+            System.out.println(stringHolder + " reversed is: " + reversedString);
+
+            if (stringHolder.equals(reversedString)) {
+                System.out.println("Is palindrome!");
+                return true;
+            } else {
+                System.out.println("Is not palindrome.");
+                return false;
+            }
+        }
 }
