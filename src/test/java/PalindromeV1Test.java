1c6e469aab1f4600074ddcea6990117e33a78833
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-05-04 00:09:22 +0000

    Pedro instantiates PalindromeV1 on PalindromeV1Test, apparently we need a new instance, Test annotation is not that magical at all.

--- src/test/java/PalindromeV1Test.java
+++ src/test/java/PalindromeV1Test.java
@@ -4,8 +4,11 @@ import static org.junit.Assert.*;
 
 public class PalindromeV1Test {
 
+    PalindromeV1 palindromeV1 = new PalindromeV1();
+
     @Test
-    public void isPalindrome() {
-        
+    public void isPalindrome() throws Exception {
+        String inputTest = "madam";
+        assertTrue(palindromeV1.isPalindrome());
     }
 }
