0601c7704f38e4207914268a57fb577180860244
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-05-04 03:14:23 +0000

    Pedro fixes PalindromeV1Test, now we have fully functional TDD

--- src/test/java/PalindromeV1Test.java
+++ src/test/java/PalindromeV1Test.java
@@ -1,5 +1,8 @@
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import static org.junit.Assert.*;
 
 public class PalindromeV1Test {
@@ -8,7 +11,13 @@ public class PalindromeV1Test {
 
     @Test
     public void isPalindrome() throws Exception {
+
         String inputTest = "madam";
+        InputStream in = new ByteArrayInputStream(inputTest.getBytes());
+        System.setIn(in);
+
+        //assertEquals(true,palindromeV1.isPalindrome());
         assertTrue(palindromeV1.isPalindrome());
+        // source https://stackoverflow.com/questions/31635698/junit-testing-for-user-input-using-scanner
     }
 }
