7a44ef0aa0bec4748f82025320d3c24ae6feae0e
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-05-05 22:28:46 +0000

    Pedro adds three tests to PalindromeV2Test

--- src/test/java/PalindromeV2Test.java
+++ src/test/java/PalindromeV2Test.java
@@ -1,10 +1,41 @@
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import static org.junit.Assert.*;
 
 public class PalindromeV2Test {
 
+    PalindromeV2 palindromeV2 = new PalindromeV2();
+
     @Test
     public void isPalindrome() {
+
+        String inputTest = "madam";
+        InputStream in = new ByteArrayInputStream(inputTest.getBytes());
+        System.setIn(in);
+        assertTrue(palindromeV2.isPalindrome());
+
+    }
+
+    @Test
+    public void isNotPalindrome() {
+
+        String inputTest = "Pedro";
+        InputStream in = new ByteArrayInputStream(inputTest.getBytes());
+        System.setIn(in);
+        assertFalse(palindromeV2.isPalindrome());
+
+    }
+
+    @Test
+    public void shouldBePalindromeWithUpperCaseInput() {
+
+        String inputTest = "Ana";
+        InputStream in = new ByteArrayInputStream(inputTest.getBytes());
+        System.setIn(in);
+        assertTrue(palindromeV2.isPalindrome());
+
     }
 }
