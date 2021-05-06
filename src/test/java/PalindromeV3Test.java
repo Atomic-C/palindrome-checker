77d8ce4695b5fd5cc48d765aee7c898252692f21
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-05-06 23:37:11 +0000

    Pedro adds failing test to PalindromeV3Test, named isPalindrome

--- src/test/java/PalindromeV3Test.java
+++ src/test/java/PalindromeV3Test.java
@@ -1,10 +1,21 @@
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import static org.junit.Assert.*;
 
 public class PalindromeV3Test {
 
+    PalindromeV3 palindromeV3 = new PalindromeV3();
+
     @Test
     public void isPalindrome() {
+
+        String inputTest = "madam";
+        InputStream in = new ByteArrayInputStream(inputTest.getBytes());
+        System.setIn(in);
+        assertTrue(palindromeV3.isPalindrome());
+
     }
 }
