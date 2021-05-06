b4f5db44c5f835392a08171408499e293a9fd310
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-05-06 23:45:12 +0000

    Pedro adds Scanner for userInput and StringBuffer class to use reverse method on Strings

--- src/main/java/PalindromeV3.java
+++ src/main/java/PalindromeV3.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class PalindromeV3 {
 
     public boolean isPalindrome() {
+
+        Scanner userInput = new Scanner(System.in); // Receive user input ere
+        System.out.println("Insert a word");
+
+
+        String stringHolder = userInput.nextLine().toLowerCase(); // Read user input "RACECAR"
+        String reversedString = "";
+        System.out.println("You've inserted: " + stringHolder); // Output user input "RACECAR"
+
+        StringBuffer sBReverse = new StringBuffer(stringHolder);
+        System.out.println(sBReverse.reverse());
         return false;
     }
 
